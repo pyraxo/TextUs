a96b2068ce451e9e72a4cc0e2f1b7acfef35b62c
pyraxo <pyraxo@pyraxo.moe>
2025-04-20 17:47:42 +0800

    Refactor and ensure input files can be seen

--- backend/app/services/chroma_db.py
+++ backend/app/services/chroma_db.py
@@ -16,7 +16,8 @@ from app.core.config import get_settings
 settings = get_settings()
 
 BASE_DIR = "./data"
-CSV_FILE_PATH = os.path.join(BASE_DIR, "faq_cat_for_embed.csv")
+INPUT_DIR = "./input"
+CSV_FILE_PATH = os.path.join(INPUT_DIR, "faq_cat_for_embed.csv")
 DB_PATH = os.path.join(BASE_DIR, "chroma_db")
 
 os.environ["GOOGLE_API_KEY"] = settings.google_api_key
